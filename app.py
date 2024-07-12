377feeba8d4ec47bcea2dbfaf8edfa3b47d7ab0b
adrianmungroo <83683300+adrianmungroo@users.noreply.github.com>
2024-07-12 19:03:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,21 +6,28 @@ import folium
 
 def showPlace(name):
     geo_info = geocode(name)
-    y = geo_info['geometry'][0].y
-    x = geo_info['geometry'][0].x
-    m = folium.Map(location=[y, x], zoom_start=8, tiles='OpenStreetMap')
+    if not geo_info['geometry'].is_empty[0]:
+        y = geo_info['geometry'][0].y
+        x = geo_info['geometry'][0].x
+        m = folium.Map(location=[y, x], zoom_start=8, tiles='OpenStreetMap')
 
-    folium.Marker(
-        location=[y, x],
-    ).add_to(m)
+        folium.Marker(
+            location=[y, x],
+        ).add_to(m)
 
-    folium.LayerControl().add_to(m)
-    
-    return m, geo_info
+        folium.LayerControl().add_to(m)
+        
+        return m, geo_info
+    else:
+        output = 'Something fucked up with the geocoding, try again i guess'
+        return(output, output)
 
 location = st.sidebar.text_input('Where do you want to go?')
 
 if location:
     m, geoinfo = showPlace(location)
-    st.title(geoinfo.address[0])
-    folium_static(m)
+    if m == 'Something fucked up with the geocoding, try again i guess':
+        st.title(m)
+    else:
+        st.title(geoinfo.address[0])
+        folium_static(m)
